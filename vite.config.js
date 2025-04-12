65518034fdf5db1c1777d19fdbe3b09021c02f83
Phuc Dai Tran <trandaiphuc.1501@gmail.com>
2025-04-12 14:54:01 +0700

    Set base path for Vite configuration to support deployment

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
+  base: '/group-expense-splitter/',
   server: {
     host: '0.0.0.0', // Listen on all network interfaces
   },
